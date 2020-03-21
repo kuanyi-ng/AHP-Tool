2f0e0845e96387239dfdfeab76458c453f1df174
KY-Ng <raionkurane@gmail.com>
2020-03-21 22:29:27 +0900

    added dynamic DOM for comparison of criterion and choices

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 const mainElem = document.querySelector('main');
-const choiceChips = document.getElementById('alternatives');
-const criteriaChips = document.getElementById('criterion');
+const choiceChips = document.getElementById('choices-input');
+const criteriaChips = document.getElementById('criterion-input');
 const inputButton = document.getElementById('input-btn');
 
+let criteriaCombo;
+let choiceCombo;
+
 const sectionTemplate = Handlebars.compile(`
-  <div class="section">
+  <div id="{{ id }}" class="section">
     <p class="caption">{{ caption }}</p>
   </div>
 `);
 
 const rangeTemplate = Handlebars.compile(`
-  <div class="row">
-    <span class="col s1 center-align">{{ left }}</span>
-    <span class="range-field col s10">
-      <input type="range" id="test5" min="-9" max="9" step="1"/>
+  <div class="row range-row">
+    <span class="col s3 center-align">{{ left }}</span>
+    <span class="range-field col s6">
+      <input id="{{ id }}" type="range" min="-9" max="9" step="1"/>
     </span>
-    <span class="col s1 center-align">{{ right }}</span>
+    <span class="col s3 center-align">{{ right }}</span>
   </div>
 `);
 
@@ -29,21 +32,28 @@ function htmlToElement(html) {
 // The first time inputButton is clicked
 inputButton.onclick = () => {
   // add "Comparison Between Criterion" Section
-  const criterionSection = sectionTemplate({'caption': 'Comparison between Criterion'});
-  mainElem.appendChild(htmlToElement(criterionSection));
+  const criterionSection = htmlToElement(sectionTemplate({
+    'id': 'criterion',
+    'caption': 'Comparison between Criterion',
+  }));
+  mainElem.appendChild(criterionSection);
 
   // add Divider
   mainElem.appendChild(htmlToElement("<div class=\"divider\"></div>"));
 
   // add "Comparison Between Choices" Section
-  const choicesSection = sectionTemplate({'caption': 'Comparison between Choices'});
-  mainElem.appendChild(htmlToElement(choicesSection));
+  const choicesSection = htmlToElement(sectionTemplate({
+    'id': 'choices',
+    'caption': 'Comparison between Choices',
+  }));
+  mainElem.appendChild(choicesSection);
 
+  updateComparisonSection();
+
+  // when inputButton is clicked again
   inputButton.onclick = () => {
     updateComparisonSection();
   };
-
-  updateComparisonSection();
 };
 
 function getChipsData(elem) {
@@ -52,7 +62,10 @@ function getChipsData(elem) {
 }
 
 function combinations(arr, r) {
-
+  let combinationIndex = combinationUtil(arr.length, r);
+  return combinationIndex.map((idx) => {
+    return idx.map(i => arr[i]);
+  });
 }
 
 function combinationUtil(l, r, result=[]) {
@@ -65,7 +78,7 @@ function combinationUtil(l, r, result=[]) {
       first.push(i);
     }
     result.push(first);
-    return experiment(l, r, result);
+    return combinationUtil(l, r, result);
   }
 
   // get the previous combination
@@ -91,7 +104,7 @@ function combinationUtil(l, r, result=[]) {
       }
       // add to result
       result.push(temp);
-      return experiment(l, r, result);
+      return combinationUtil(l, r, result);
     }
   }
 }
@@ -101,7 +114,55 @@ function updateComparisonSection() {
   let choices = getChipsData(choiceChips);
   // get Criterion
   let criterion = getChipsData(criteriaChips);
-  // create Range
-  console.log(choices);
-  console.log(criterion);
+
+  // empty inputs
+  if ((choices.length === 0) || (criterion.length === 0)) {
+    return;
+  }
+
+  // obtain DOM of choicesSection & criterionSection
+  const criterionSection = document.getElementById('criterion');
+  const choicesSection = document.getElementById('choices');
+
+  // create Reference
+  const criterionReference = htmlToElement(rangeTemplate({
+    'left': 'Left is more Important',
+    'right': 'Right is more Important'
+  }));
+
+  const choicesReference = htmlToElement(rangeTemplate({
+    'left': 'Left is Better',
+    'right': 'Right is Better'
+  }));
+
+  // create Range for Comparison between each Criteria
+  criteriaCombo = combinations(criterion, 2);
+  criterionSection.appendChild(htmlToElement("<p class=\"caption\">Reference: </p>"));
+  criterionSection.appendChild(criterionReference);
+  for (let combo of criteriaCombo) {
+    let cleft = criterion.indexOf(combo[0]);
+    let cright = criterion.indexOf(combo[1]);
+    let id = `c-${cleft}-${cright}`;
+    let cRange = htmlToElement(rangeTemplate({'id': id, 'left': combo[0], 'right': combo[1]}));
+    criterionSection.appendChild(cRange);
+  }
+
+  choiceCombo = combinations(choices, 2);
+  choicesSection.appendChild(htmlToElement("<p class=\"caption\">Reference: </p>"));
+  choicesSection.appendChild(choicesReference);
+  for (let c of criterion) {
+    choicesSection.appendChild(htmlToElement(`<p class="caption">${c}</p>`));
+    for (let combo of choiceCombo) {
+      let cIdx = criterion.indexOf(c);
+      let aleft = choices.indexOf(combo[0]);
+      let aright = choices.indexOf(combo[1]);
+      let id = `c-${cIdx}-a-${aleft}-${aright}`;
+      let aRange = htmlToElement(rangeTemplate({'id': id, 'left': combo[0], 'right': combo[1]}));
+      choicesSection.appendChild(aRange);
+    }
+  }
+  // initialize the ranges that are newly added
+  M.Range.init(document.querySelectorAll("input[type=range]"));
+  // console.log(choices);
+  // console.log(criterion);
 }
--- js/materialize.js
+++ js/materialize.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-   var choiceChips = document.getElementById('alternatives');
+   var choiceChips = document.getElementById('choices-input');
    var choiceChipsInstances = M.Chips.init(choiceChips, {
      placeholder: "Add your choice",
      secondaryPlaceholder: "+ choice",
      onChipAdd: null,
    });
 
-   var criteriaChips = document.getElementById('criterion')
+   var criteriaChips = document.getElementById('criterion-input');
    var criteriaChipsInstances = M.Chips.init(criteriaChips, {
      placeholder: "Add your criteria",
      secondaryPlaceholder: "+ criteria",
