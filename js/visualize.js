71e93f8b5162cc2260f1547cacc54abb211913da
kuanyi-ng <raionkurane@gmail.com>
2022-02-05 15:21:29 +0900

    change chart color to grey;

--- js/visualize.js
+++ js/visualize.js
@@ -6,46 +6,49 @@ function criteriaWeightBar(canvas_id, criteriaWeight) {
     y.push(criteriaWeight[criteria]);
   }
 
+  const borderColor = 'rgb(33, 33, 33)';
+  const backgroundColor = rgbToRgba(borderColor, 0.2);
+
   const ctx = document.getElementById(canvas_id).getContext('2d');
 
   let bar = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: x,
-        datasets: [{
-            data: y,
-            backgroundColor: 'rgba(33, 33, 33, 0.2)',
-            borderColor: 'rgb(33, 33, 33)',
-            borderWidth: 1,
-        }]
+    type: 'bar',
+    data: {
+      labels: x,
+      datasets: [{
+        data: y,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1,
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Importance of each Criteria'
       },
-      options: {
-        title: {
-              display: true,
-              text: 'Importance of each Criteria'
-        },
-        legend: {
-          display: false
-        },
-        scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero:true,
-                  min: 0,
-                  // max: 1
-              }
-          }]
-        }
+      legend: {
+        display: false
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+            // max: 1
+          }
+        }]
       }
+    }
   });
 }
 
 function choiceScoreBar(canvas_id, choices, finalScore, criterion) {
   let colors;
   if (criterion.length > 9) {
-    colors = palette('tol-rainbow', criterion.length).map(color => `#${color}`);
+    colors = palette('tol-rainbow', criterion.length).map(color => hexToRgb(color));
   } else {
-    colors = palette('cb-BuGn', criterion.length).map(color => `#${color}`);
+    colors = palette('cb-Greys', criterion.length).map(color => hexToRgb(color));
   }
 
   let datasets = [];
@@ -58,8 +61,9 @@ function choiceScoreBar(canvas_id, choices, finalScore, criterion) {
     datasets.push({
       label: criterion[c],
       data: data,
-      backgroundColor: colors[c],
+      backgroundColor: rgbToRgba(colors[c], 0.2),
       borderColor: colors[c],
+      borderWidth: 1,
     });
   }
 
@@ -67,7 +71,7 @@ function choiceScoreBar(canvas_id, choices, finalScore, criterion) {
 
   const ctx = document.getElementById(canvas_id).getContext('2d');
 
-  let stackedBar = new Chart (ctx, {
+  let stackedBar = new Chart(ctx, {
     type: 'bar',
     data: {
       labels: choices,
@@ -93,4 +97,25 @@ function choiceScoreBar(canvas_id, choices, finalScore, criterion) {
       },
     }
   });
+}
+
+function rgbToRgba(rgbString, alpha) {
+  // rgb_str: rgb(R, G, B)
+  // 
+  // return: rgba(R, G, B, alpha)
+  let rgbaValues = rgbString.slice(4, -1).split(',').map(value => value.trim());
+  rgbaValues.push(alpha);
+
+  return `rgba(${rgbaValues.join(',')})`;
+}
+
+function hexToRgb(hex) {
+  // hex: rrggbb
+  //
+  // return: rgb(R, G, B)
+  const red = parseInt(hex.slice(0, 2), 16);
+  const green = parseInt(hex.slice(2, 4), 16);
+  const blue = parseInt(hex.slice(4, 6), 16);
+
+  return `rgb(${red}, ${green}, ${blue})`;
 }
